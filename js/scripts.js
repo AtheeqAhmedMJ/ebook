138640fe47e5d7a2a49b0fb101880810acf35a52
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2024-11-23 11:41:01 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     enterButton.addEventListener('click', () => {
+        window.location.href = 'ebook.html';
         introScreen.style.display = 'none';
         mainContent.style.display = 'flex';
         bgMusic.play();
